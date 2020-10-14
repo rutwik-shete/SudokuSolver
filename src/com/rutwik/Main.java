ea574a80115b8572ff1abe384d20ee8bbf726039
rutwik-shete <rutwik.shete@gmail.com>
2020-10-14 18:59:31 +0530

    Final Product

--- src/com/rutwik/Main.java
+++ src/com/rutwik/Main.java
@@ -8,80 +8,47 @@ public class Main {
     public static Set<Integer> numbersAvailable = new HashSet<>(new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9)));
     public static List<List<Set<Integer>>> maybe = new ArrayList<>();
     public static int iterration = 0;
+    public static int checkResemblance = 81;
+    public static int checkResemblanceTemp;
     public static void main(String[] args) {
 
-        int[] row0 = {5,3,0,0,7,0,0,0,0};
-        int[] row1 = {6,0,0,1,9,5,0,0,0};
-        int[] row2 = {0,9,8,0,0,0,0,6,0};
-        int[] row3 = {8,0,0,0,6,0,0,0,3};
-        int[] row4 = {4,0,0,8,0,3,0,0,1};
-        int[] row5 = {7,0,0,0,2,0,0,0,6};
-        int[] row6 = {0,6,0,0,0,0,2,8,0};
-        int[] row7 = {0,0,0,4,1,9,0,0,5};
-        int[] row8 = {0,0,0,0,8,0,0,7,9};
+//        int[] row0 = {0,8,0,0,0,6,0,0,3};
+//        int[] row1 = {0,6,0,0,7,0,2,0,0};
+//        int[] row2 = {0,0,4,0,1,0,0,9,0};
+//        int[] row3 = {0,5,0,0,0,0,8,0,1};
+//        int[] row4 = {0,0,0,0,0,0,0,0,0};
+//        int[] row5 = {0,1,3,0,0,0,0,6,0};
+//        int[] row6 = {0,0,5,0,0,9,0,0,8};
+//        int[] row7 = {0,0,0,0,2,0,0,0,0};
+//        int[] row8 = {0,0,6,7,0,0,3,0,2};
+
+        int[] row0 = {8,0,0,0,0,0,0,0,0};
+        int[] row1 = {0,0,3,6,0,0,0,0,0};
+        int[] row2 = {0,7,0,0,9,0,2,0,0};
+        int[] row3 = {0,5,0,0,0,7,0,0,0};
+        int[] row4 = {0,0,0,0,4,5,7,0,0};
+        int[] row5 = {0,0,0,1,0,0,0,3,0};
+        int[] row6 = {0,0,1,0,0,0,0,6,8};
+        int[] row7 = {0,0,8,5,0,0,0,1,0};
+        int[] row8 = {0,9,0,0,0,0,4,0,0};
+
+//        int[] row0 = {5,3,0,0,7,0,0,0,0};
+//        int[] row1 = {6,0,0,1,9,5,0,0,0};
+//        int[] row2 = {0,9,8,0,0,0,0,6,0};
+//        int[] row3 = {8,0,0,0,6,0,0,0,3};
+//        int[] row4 = {4,0,0,8,0,3,0,0,1};
+//        int[] row5 = {7,0,0,0,2,0,0,0,6};
+//        int[] row6 = {0,6,0,0,0,0,2,8,0};
+//        int[] row7 = {0,0,0,4,1,9,0,0,5};
+//        int[] row8 = {0,0,0,0,8,0,0,7,9};
 
         int[][] sudoku = {row0,row1,row2,row3,row4,row5,row6,row7,row8};
 
-        printSudoku(sudoku);
-
         solveSudoku(sudoku);
-
-        System.out.println();
-        System.out.println("---------------------X---------------------X---------------------X---------------------");
-        System.out.println();
-
-        System.out.println("Answer : ");
-        System.out.println();
-
-        printSudoku(sudoku);
-
-        System.out.println("Iteration Number : "+iterration);
-
     }
 
-    public static boolean isSudokuSolved(int[][] sudoku){
-        for(int row = 0 ; row < sudoku.length ; row++){
-            for(int col = 0 ; col < sudoku[row].length ; col++){
-                if(sudoku[row][col] == 0)
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    public static void solveSudoku(int[][] sudoku){
-
-//        System.out.println();
-//        System.out.println("---------------------X---------------------X---------------------X---------------------");
-//        System.out.println();
-//
-//        printSudoku(sudoku);
-
-        checkRow(sudoku);
-        checkColumn(sudoku);
-
-        for(int row = 0 ; row <= sudoku.length-3 ; row+=3){
-            checkBox(sudoku,row,0,row + 2,2);
-            checkBox(sudoku,row,3,row + 2,5);
-            checkBox(sudoku,row,6,row + 2,8);
-        }
-
-        fillTheBox(sudoku);
-
-//        printMaybe();
-//
-//        printSudoku(sudoku);
-
-        iterration++;
-
-//        System.out.println("Iteration Number : "+iterration);
-
-        if(!isSudokuSolved(sudoku))
-            solveSudoku(sudoku);
-
-    }
-
-    public static void fillTheBox(int[][] sudoku){
+    public static int fillTheBox(int[][] sudoku){
+        int countEmptyMaybe = 0;
         Set<Integer> temp ;
         for(int row = 0 ; row < sudoku.length ; row++){
             for(int col = 0 ; col < sudoku[row].length ; col++){
@@ -90,8 +57,12 @@ public class Main {
                         sudoku[row][col] = number;
                     }
                 }
+                else if((temp = maybe.get(row).get(col)).isEmpty()){
+                    countEmptyMaybe++;
+                }
             }
         }
+        return countEmptyMaybe;
     }
 
     public static void checkBox(int[][] sudoku , int rowStart , int colStart , int rowEnd , int colEnd){
@@ -197,6 +168,8 @@ public class Main {
 
     public static  void printSudoku(int[][] sudoku){
 
+        System.out.println(" ----------------------------------X ----------------------------------X ----------------------------------X");
+
         String colSpacer = " | ";
         String rowSpacer = " *---*---*---*---*---*---*---*---*---*";
 
@@ -243,5 +216,120 @@ public class Main {
         System.out.println();
     }
 
+    public static boolean tryPrediction(int[][] sudoku){
+        Set<Integer> temp ;
+        for(int row = 0 ; row < sudoku.length ; row++){
+            for(int col = 0 ; col < sudoku[row].length ; col++){
+                if((temp = maybe.get(row).get(col)).size() == 2){
+                    for(int number : temp) {
+                        sudoku[row][col] = number;
+                        if(solveSudoku(returnCopy(sudoku))){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
+    public static boolean solveSudoku(int[][] sudoku){
+        iterration++;
+        System.out.println(iterration);
+        checkRow(sudoku);
+        checkColumn(sudoku);
+        for(int row = 0 ; row <= sudoku.length-3 ; row+=3){
+            checkBox(sudoku,row,0,row + 2,2);
+            checkBox(sudoku,row,3,row + 2,5);
+            checkBox(sudoku,row,6,row + 2,8);
+        }
+        checkResemblanceTemp = fillTheBox(sudoku);
+
+        if(checkResemblanceTemp==checkResemblance && isZeroInSudoku(sudoku)){
+            checkResemblance = checkResemblanceTemp;
+            return tryPrediction(sudoku);
+        }
+        else {
+            printSudoku(sudoku);
+            checkResemblance = checkResemblanceTemp;
+
+            if (isZeroInSudoku(sudoku)) {
+                if (checkResemblance == 81)
+                    return false;
+                return solveSudoku(sudoku);
+            }
+            System.out.println("Resemblance : " + checkResemblance);
+            return isSudokuSolved(sudoku);
+        }
+    }
+
+    public static int[][] returnCopy(int [][] sudoku){
+        int[][] Copy = new int[sudoku.length][sudoku.length];
+        for(int row = 0 ; row < sudoku.length ; row++){
+            for(int col = 0 ; col < sudoku[row].length ; col++){
+                Copy[row][col] = sudoku[row][col];
+            }
+        }
+        return Copy;
+    }
+
+    public static boolean isZeroInSudoku(int[][] sudoku){
+        for(int row = 0 ; row < sudoku.length ; row++){
+            for(int col = 0 ; col < sudoku[row].length ; col++){
+                if(sudoku[row][col] == 0)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSudokuSolved(int[][] sudoku){
+        for(int row = 0 ; row < sudoku.length ; row++){
+            for(int col = 0 ; col < sudoku[row].length ; col++){
+                if(sudoku[row][col] != 0){
+                    if(!isSafe(sudoku,row,col,sudoku[row][col])){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static int usedInRow(int[][] grid, int row, int num) {
+        int count=0;
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[row][i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int usedIncol(int[][] grid, int col, int num) {
+        int count=0;
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][col] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int usedInBox(int[][] grid, int row1Start, int col1Start, int num) {
+        int count=0;
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
+                if (grid[row + row1Start][col + col1Start] == num) {
+                    count++;
+                }
+        return count;
+
+    }
+
+    public static boolean isSafe(int[][] grid, int row, int col, int num) {//is it safe to place that number at that position, might not be correct nut just safe
+
+        return (usedIncol(grid, col, num)==1 && usedInRow(grid, row, num)==1 && usedInBox(grid, row - row % 3, col - col % 3, num)==1);
+
+    }
 }
